9dc78d00bd8015058fefd9ccb40c413c380c02d1
Med Aduh <im.ramidem@gmail.com>
2018-09-09 15:07:42 +0800

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>I am <a href="https://twitter.com/ramidem">@ramidem</a></p>
+    <p>Passionate about Web Development.</p>
+  </div>
+)
+
+export default IndexPage
